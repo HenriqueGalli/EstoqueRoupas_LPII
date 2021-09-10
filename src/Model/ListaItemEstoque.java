38651a3d27d7f9b179be187630f76d915530c27e
Renan Franca <renanofranca@hotmail.com>
2021-09-10 17:15:02 -0300

    Adição escrita e leitura do arquivo txt

--- src/Model/ListaItemEstoque.java
+++ src/Model/ListaItemEstoque.java
@@ -1,4 +1,13 @@
 package Model;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -6,20 +15,107 @@ public class ListaItemEstoque {
 
     private static final ArrayList<ItemEstoque> itens = new ArrayList<ItemEstoque>();
 
-    public ArrayList<ItemEstoque> getList(){
+    public ArrayList<ItemEstoque> getList() {
         return itens;
     }
 
-    public int getListSize(){
+    public int getListSize() {
         return itens.size();
     }
 
-    public void addListaEstoque(ItemEstoque item){
+    public void addListaEstoque(ItemEstoque item) {
         itens.add(item);
     }
 
-    public void excludeListaEstoque(int item){
+    public void excludeListaEstoque(int item) {
         itens.remove(item);
     }
 
+    public void salvarArquivo() throws IOException {
+        FileWriter arq = new FileWriter("src//Arquivo.txt");
+        PrintWriter gravarArq = new PrintWriter(arq);
+        for (int i = 0; i < getListSize(); i++) {
+            gravarArq.printf(itens.get(i).getCodigoItem() + ";");
+            gravarArq.printf(itens.get(i).getDataEntrada() + ";");
+            gravarArq.printf(itens.get(i).getLocalCompra() + ";");
+            gravarArq.printf(itens.get(i).getTipoRoupa() + ";");
+            gravarArq.printf(itens.get(i).getMarcaRoupa() + ";");
+            gravarArq.printf(itens.get(i).getCaracteristicaRoupa() + ";");
+            gravarArq.printf(itens.get(i).getTamanhoPeca() + ";");
+            gravarArq.printf(itens.get(i).getCorPeca() + ";");
+            gravarArq.printf(itens.get(i).getValorEtiqueta() + ";");
+            gravarArq.printf(itens.get(i).getValorPago() + ";");
+            gravarArq.printf(itens.get(i).getPrecoSugerido() + "\n");
+        }
+        arq.close();
+    }
+
+    public void lerArquivo() throws IOException, NumberFormatException, ParseException {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        FileReader arq = new FileReader("src//Arquivo.txt");
+        BufferedReader lerArq = new BufferedReader(arq);
+        String linha = lerArq.readLine();        
+        if (linha != null) {
+            while (linha != null) {
+                String[] produto = linha.split(";");
+                TamanhoPeca tamanhoPeca = lerTamanho(produto[6]);
+                CorPeca corPeca = lerCor(produto[7]);
+                ItemEstoque item = new ItemEstoque(Integer.parseInt(produto[0]), formato.parse(produto[1]), produto[2],
+                        produto[3], produto[4], produto[5],tamanhoPeca, corPeca, Double.parseDouble(produto[8]), 
+                        Double.parseDouble(produto[9]), Double.parseDouble(produto[10]));
+                addListaEstoque(item);
+                linha = lerArq.readLine();
+            }
+        }
+    }
+
+    private TamanhoPeca lerTamanho(String tamanho){
+        TamanhoPeca tamanhoPeca = null;
+        switch (tamanho) {
+            case "PEQUENO":
+                tamanhoPeca = TamanhoPeca.PEQUENO;
+                break;
+
+            case "MEDIO":
+                tamanhoPeca = TamanhoPeca.MEDIO;
+                break;
+
+            case "GRANDE":
+                tamanhoPeca = TamanhoPeca.GRANDE;
+                break;
+
+            default:
+                break;
+        }
+        return tamanhoPeca;
+    }
+
+    private CorPeca lerCor(String tamanho){
+        CorPeca corPeca = null;
+        switch (tamanho) {
+            case "AZUL":
+                corPeca = CorPeca.AZUL;
+                break;
+
+            case "BRANCA":
+                corPeca = CorPeca.BRANCA;
+                break;
+
+            case "PRETA":
+                corPeca = CorPeca.PRETA;
+                break;
+
+            case "LARANJA":
+                corPeca = CorPeca.LARANJA;
+                break;
+
+            case "CINZA":
+                corPeca = CorPeca.CINZA;
+                break;
+
+            default:
+                break;
+        }
+        return corPeca;
+    }
 }
--- src/View/IterfaceUsuario.java
+++ src/View/IterfaceUsuario.java
@@ -1,55 +1,66 @@
 package View;
+
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.Scanner;
+import Model.ListaItemEstoque;
 
 import Controller.FuncionalidadesEstoque;
 
 public class IterfaceUsuario {
 
-    Scanner scannerMenu = new Scanner(System.in);
-    FuncionalidadesEstoque metodosEstoque = new FuncionalidadesEstoque();
+  Scanner scannerMenu = new Scanner(System.in);
+  FuncionalidadesEstoque metodosEstoque = new FuncionalidadesEstoque();
+  ListaItemEstoque listaItemEstoque = new ListaItemEstoque();
+  int cont = 0;
 
-    public void criarMenu() throws ParseException{
-        System.out.println("|------------------- Menu de Ferramentas -------------------|");
-        System.out.println("|                  1 - Adicionar Tarefas                    |");
-        System.out.println("|                  2 - Listar todas as Tarefas              |");
-        System.out.println("|                  3 - Excluir Tarefa                       |");
-        System.out.println("|                  4 - Editar Tarefa                        |");
-        System.out.println("|                  5 - Sair                                 |");
-        System.out.println("|-----------------------------------------------------------|");
-
-        String opcao = scannerMenu.nextLine();
-
-        switch (opcao) {
-          case "1":   
-            metodosEstoque.criarNovoItem();
-            criarMenu();
-            break;
-
-          case "2":
-            metodosEstoque.listarEstoqueDetalhado(metodosEstoque.listarEstoque());
-            criarMenu();
-            break;
-
-          case "3":
-            metodosEstoque.excluirProduto(metodosEstoque.listarEstoque());
-            criarMenu();
-            break;
-
-          case "4":
-            metodosEstoque.editarProtudo();
-            criarMenu();
-            break;
-
-          case "5":
-            System.exit(0);
-            criarMenu();
-          break;
-            
-          default:
-            System.out.println("Opção inválida\nSelecione novamente");
-            criarMenu();
-        }
+  public void criarMenu() throws ParseException, IOException {
+    System.out.println("|------------------- Menu de Ferramentas -------------------|");
+    System.out.println("|                  1 - Adicionar Tarefas                    |");
+    System.out.println("|                  2 - Listar todas as Tarefas              |");
+    System.out.println("|                  3 - Excluir Tarefa                       |");
+    System.out.println("|                  4 - Editar Tarefa                        |");
+    System.out.println("|                  5 - Sair                                 |");
+    System.out.println("|-----------------------------------------------------------|");
+  
+    if(cont == 0){
+      listaItemEstoque.lerArquivo();
+      cont++;
     }
-       
+
+    String opcao = scannerMenu.nextLine();
+
+    switch (opcao) {
+      case "1":
+        metodosEstoque.criarNovoItem();
+        criarMenu();
+        break;
+
+      case "2":
+        metodosEstoque.listarEstoqueDetalhado(metodosEstoque.listarEstoque());
+        criarMenu();
+        break;
+
+      case "3":
+        metodosEstoque.excluirProduto(metodosEstoque.listarEstoque());
+        criarMenu();
+        break;
+
+      case "4":
+        metodosEstoque.editarProtudo();
+        criarMenu();
+        break;
+
+      case "5":
+        listaItemEstoque.salvarArquivo();
+        System.exit(0);
+        criarMenu();
+        break;
+
+      default:
+        System.out.println("Opção inválida\nSelecione novamente");
+        criarMenu();
+    }
+  }
+
 }
